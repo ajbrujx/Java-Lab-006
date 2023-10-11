00e35941971280eaa13ec63b3993ff09877c56ab
Atticus Blanco <atticusbrujx@gmail.com>
2023-10-11 00:21:12 -0700

    Part 3 ---OPERATIONAL--- >>>MODIFICATIONS --Lab006.java   -CREATED javadoc for Lab006 constructor   -CREATED javadoc for isDivisible method   -CREATED javadoc for printToTerminal method   --isDivisible     -simplified if tree, return boolean result of mod operation --Main.java   -CREATED javadoc for main method >>>DEFICITS -- >>>BUGS -- >>>THROWS --

--- src/Lab006.java
+++ src/Lab006.java
@@ -3,19 +3,31 @@ public class Lab006 {
     private final int n;
     private final int m;
 
-    public Lab006(int n, int m) {   //is this a setter???
+    /**
+     * Constructs a Lab006 object
+     * @author Atticus Blanco
+     * @param n value assigned to the instance variable (n)
+     * @param m value assigned to the instance variable (m)
+     */
+    public Lab006(int n, int m) {
         this.n = n;
         this.m = m;
     }
 
+    /**
+     * Determines if variable (n) is divisible by variable (m)
+     * @author Atticus Blanco
+     * @return {@code true} if n is divisible by m, else returns {@code false}
+     */
     public boolean isDivisible() {
-        if (n % m == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return n % m == 0;
     }
 
+    /**
+     * Prints to terminal the divisibility of two numbers
+     * @author Atticus Blanco
+     * @see #isDivisible()
+     */
     public void printToTerminal(){
         if (isDivisible()) {
             System.out.println("Divisible");
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 
 public class Main {
 
+    /**
+     * Prompts user for a dividend and divisor, stores the results to local variables.
+     * Lab006 object is created to test divisibility of local variables, and then print results.
+     * @author Atticus Blanco
+     */
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
@@ -11,7 +16,6 @@ public class Main {
         int divisor = Integer.parseInt(s.nextLine());
 
         Lab006 lab006 = new Lab006(dividend, divisor);
-
         lab006.isDivisible();
         lab006.printToTerminal();
     }
